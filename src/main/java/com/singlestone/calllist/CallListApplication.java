46a432d7af821c15e0550229e182576b7b77be74
Brian Weedon <bweedon03@gmail.com>
2021-10-23 22:44:30 -0400

    Initial commit with spring boot template
    Initial commit with spring boot template created using https://start.spring.io/. Added in H2, Spring JPA, and the Spring Web Plugins

---
+++ src/main/java/com/singlestone/calllist/CallListApplication.java
@@ -0,0 +1,13 @@
+package com.singlestone.calllist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CallListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/singlestone/calllist/CallListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.singlestone.calllist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CallListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
